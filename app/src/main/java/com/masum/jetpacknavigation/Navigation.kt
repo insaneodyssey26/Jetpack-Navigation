5f198864d2f1a067c008a2b056f44ad2f0bbab06
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-10-21 21:15:14 +0530

    Added screens in sealed class

---
+++ app/src/main/java/com/masum/jetpacknavigation/Navigation.kt
@@ -0,0 +1,17 @@
+package com.masum.jetpacknavigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Navigation () {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screen.MainScreen.route) {
+        composable (route = Screen.MainScreen.route) {
+
+        }
+    }
+}
