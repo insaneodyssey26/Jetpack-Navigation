8acddfca0dd1e5189d53b480d887bfc8190cd9ec
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-10-24 16:10:26 +0530

    feat: splash screen composable with animation

--- app/src/main/java/com/masum/jetpacknavigation/SplashScreen.kt
+++ app/src/main/java/com/masum/jetpacknavigation/SplashScreen.kt
@@ -1,30 +1,62 @@
 package com.masum.jetpacknavigation
 
+import android.view.animation.OvershootInterpolator
+import android.window.SplashScreen
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.internal.composableLambda
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-
-@Composable
-fun NavigationWithSplashScreen () {
-    val navController = rememberNavController()
-        NavHost(navController = navController, startDestination = 'splash_screen') {
-
-        }
-}
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashImage (modifier: Modifier = Modifier) {
-    val navController by rememberNavController()
+    val navController = rememberNavController()
     NavHost(navController = navController, startDestination = "splash_screen") {
         composable ("splash_screen") {
-
+            SplashScreen(navController)
         }
         composable ("main_screen") {
 
         }
+    }
+}
+
+@Composable
+fun SplashScreen (navController: NavController) {
+    val scale = remember {
+        Animatable(0f)
+    }
+    LaunchedEffect(true) {
+        scale.animateTo(
+            targetValue = 0.3f,
+            animationSpec = tween(
+                durationMillis = 500,
+                easing = {
+                    OvershootInterpolator(2f).getInterpolation(it)
+                }
+            )
+        )
+        delay(3000L)
+        navController.navigate("main_screen")
+    }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(painterResource(R.drawable.me),
+            contentDescription = "Splash Image"
+        )
     }
 }
